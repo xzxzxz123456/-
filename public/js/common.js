37148e09a86de539fe58076809c4c5137765215d
xz <xzxz@qq.com>
2017-06-15 18:31:47 +0800

    03登陆退出,不登陆无法访问首页

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,32 @@
 
-	NProgress.start();
+NProgress.start();
 
-	NProgress.done();
+NProgress.done();
 
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+$('.navs ul').prev('a').on('click', function () {
+	$(this).next().slideToggle();
+});
+
+
+
+//退出登陆功能实现 
+$("#logout").on("click", function () {
+	$.ajax({
+		url: '/api/logout',
+		type: "post",
+		success: function (data) {
+			if (data.code == 200) {
+				location.href = '/login'
+			}
+		}
+	})
+
+})
+// 不登陆无法访问 首页
+if ( location.pathname !== "/login" && !$.cookie('PHPSESSID') ) {
+
+	location.href = '/login'
+
+
+}
+	//  console.log( $.cookie() );
