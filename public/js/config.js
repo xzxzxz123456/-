190f308538a12667c88a11517a838662f1af333f
xz <xzxz@qq.com>
2017-06-16 15:28:23 +0800

    04-2成功

--- public/js/config.js
+++ public/js/config.js
@@ -1,6 +1,12 @@
+
 require.config({
-    baseUrl:"/public",
-    paths:{
-        jquery:'assets/jquery/jquery'
+    baseUrl : '/public',
+
+    paths : {
+        jquery : 'assets/jquery/jquery.min',
+        bootstrap : 'assets/bootstrap/js/bootstrap.min',
+        nprogress : 'assets/nprogress/nprogress',
+        echarts : 'assets/echarts/echarts.min',
+        jqueryCookie : 'assets/jquery-cookie/jquery.cookie',
     }
 })
