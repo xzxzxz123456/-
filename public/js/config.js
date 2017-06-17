759a77db709e6e6c1807a2a2d2a405c30aca98f8
xz <xzxz@qq.com>
2017-06-17 16:58:45 +0800

    07.讲师查看详情完成

--- public/js/config.js
+++ public/js/config.js
@@ -10,6 +10,13 @@ require.config({
         echarts : 'assets/echarts/echarts.min',
         jqueryCookie : 'assets/jquery-cookie/jquery.cookie',
         template:'assets/artTemplate/template-web'
+    },
+    // 模块之间的依赖配置 
+    // 因为 定义 模板  依赖项是 顺序是 不确定的 所以 要配置下
+    shim:{
+        bootstrap:{
+            deps:['jquery']
+        }
     }
 })
 
--- public/js/teacher/list.js
+++ public/js/teacher/list.js
@@ -1,5 +1,6 @@
 
-define(['jquery','template'],function($,template){
+define(['jquery','template','bootstrap'],function($,template){
+    //  获取讲师列表
     $.ajax({
         url:'/api/teacher',
         type:'get',
@@ -7,7 +8,28 @@ define(['jquery','template'],function($,template){
             if(data.code === 200){
                 var  html = template( 'teacher_list_tpl' ,data)
                 $("#teacherList").html(html)
+
+                // 查看讲师详情
+                $(".btn-xs").on('click',function(){
+                    $.ajax({
+                        url:'/api/teacher/view?tc_id=' + $(this).parent().data('id'),
+                        type:"get",
+                        success:function(data){
+                            if(data.code === 200){
+                                var html = template("teacher_view_tpl",data.result)
+                                console.log(html)
+                                $("#xiangqing").html(html)
+                                //让模态框显示
+                                $("#teacherModal").modal();
+                            }
+                        }
+                    })
+                })
             }
         }
     })
+
+    
+
+
 })
